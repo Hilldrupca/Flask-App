1ecc04c2e6460c506f5514c32733a85d0d77a2e1
Chris-desktop <hilldrupca@gmail.com>
2020-05-15 23:43:53 -0400

    Added route() for recipe_builder.html

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+@app.route('/recipebuilder/')
+def recipe_builder():
+    return render_template('recipe-builder.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
