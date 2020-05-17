7e48429cd7bd8a7d5a850f1d7deef3f494966a29
Chris-desktop <hilldrupca@gmail.com>
2020-05-16 20:16:14 -0400

    added routes for account, sign-in, and create account

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def home_page():
 def recipe_builder():
     return render_template('recipe-builder.html')
 
+@app.route('/account/')
+def account():
+    return render_template('account.html')
+
+@app.route('/sign-in/')
+def sign_in():
+    return render_template('sign-in.html')
+
+@app.route('/create-account/')
+def create_account():
+    return render_template('create-account.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
