5908378368bf5fbc193e31e88fb692a468512419
Chris-desktop <hilldrupca@gmail.com>
2020-05-17 21:36:20 -0400

    Changed urls to canonical names.
    Removed sign-in, and create-account route()'s.

--- app.py
+++ app.py
@@ -4,23 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
+    print(url_for('recipe_builder'))
     return render_template('home.html')
 
-@app.route('/recipebuilder/')
+@app.route('/recipebuilder')
 def recipe_builder():
     return render_template('recipe-builder.html')
 
-@app.route('/account/')
+@app.route('/account')
 def account():
     return render_template('account.html')
 
-@app.route('/sign-in/')
-def sign_in():
-    return render_template('sign-in.html')
-
-@app.route('/create-account/')
-def create_account():
-    return render_template('create-account.html')
+@app.route('/test')
+def test():
+    return 'test'
 
 if __name__ == '__main__':
     app.run(debug=True)
