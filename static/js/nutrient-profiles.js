fb52a957c817d3de6a1ecfef1b634fc94b766687
Chris-desktop <hilldrupca@gmail.com>
2020-05-21 21:18:37 -0400

    init

---
+++ static/js/nutrient-profiles.js
@@ -0,0 +1,109 @@
+// *****Template*****
+// Profile:
+// Calories, kcal:
+// Fats:
+// Trans fats:
+// Saturated fats:
+// Monounsaturated fats:
+// Polyunsaturated fats:
+// Cholesterol:
+// Carbohydrates:
+// Fiber:
+// Protein:
+// Vitamin A:
+// Thiamin (B1):
+// Riboflavin (B2):
+// Niacin (B3):
+// Pantothenic acid (B5):
+// Vitamin B6:
+// Folate (B9):
+// Vitamin B12:
+// Vitamin C:
+// Vitamin D:
+// Vitamin E:
+// Vitamin K:
+// Choline:
+// Calcium:
+// Copper:
+// Iron:
+// Manganese:
+// Magnesium:
+// Phosphorus:
+// Potassium:
+// Selenium:
+// Sodium:
+// Zinc:
+
+
+
+var profiles = [{
+    Profile: "Female 2000 calories (40% fat, 40% carbs, 20% protein), ages 19+",
+    Calories: 2000,
+    Fats: 88.9,
+    "Trans fats": 0,
+    "Saturated fats": "n/a",
+    "Monounsaturated fats": "n/a",
+    "Polyunsaturated fats": "n/a",
+    Cholesterol: "n/a",
+    Carbohydrates: 200,
+    Fiber: 28,
+    Protein: 100,
+    "Vitamin A": 700,
+    "Thiamin (B1)": 1.1,
+    "Riboflavin (B2)": 1.1,
+    "Niacin (B3)": 14,
+    "Pantothenic acid (B5)": 5,
+    "Vitamin B6": 1.5,
+    "Folate (B9)": 400,
+    "Vitamin B12": 2.4,
+    "Vitamin C": 75,
+    "Vitamin D": 600,
+    "Vitamin E": 15,
+    "Vitamin K": 120,
+    Choline: 2300,
+    Calcium: 1200,
+    Copper: [.9, 10],
+    Iron: 18,
+    Manganese: 2.3,
+    Magnesium: 420,
+    Phosphorus: 700,
+    Potassium: 4700,
+    Selenium: 55,
+    Sodium: 1500,
+    Zinc: 11
+},{
+    Profile: "Generic US Male 2000 calores (40% fat, 40% carbs, 20% protein), ages 19+",
+    Calories: 2000,
+    Fats: 88.9,
+    "Trans fats": 0,
+    "Saturated fats": "n/a",
+    "Monounsaturated fats": "n/a",
+    "Polyunsaturated fats": "n/a",
+    Cholesterol: "n/a",
+    Carbohydrates: 200,
+    Fiber: 28,
+    Protein: 100,
+    "Vitamin A": 900,
+    "Thiamin (B1)": 1.2,
+    "Riboflavin (B2)": 1.3,
+    "Niacin (B3)": 16,
+    "Pantothenic acid (B5)": 5,
+    "Vitamin B6": 1.7,
+    "Folate (B9)": 400,
+    "Vitamin B12": 2.4,
+    "Vitamin C": 90,
+    "Vitamin D": 600,
+    "Vitamin E": 15,
+    "Vitamin K": 110,
+    Choline: 2300,
+    Calcium: 1200,
+    Copper: [.9, 10],
+    Iron: 18,
+    Manganese: 2.3,
+    Magnesium: 420,
+    Phosphorus: 700,
+    Potassium: 4700,
+    Selenium: 55,
+    Sodium: 1500,
+    Zinc: 11
+}];
